35e183ab15bd8d70c96c2b868102e91e3fe26fc3
Leonardo Werlang <werlangleonardo@gmail.com>
2019-04-13 18:04:51 -0300

    :hammer: 02 | Adiciona relogios relativos
    Soma um no estado do relogio do roteador quando:
    :white_check_mark: Usuario escreve uma mensagem
    :white_check_mark: Envia a mensagem para outro cliente
    :white_check_mark: Recebe uma mensagem

--- t2/client.rb
+++ t2/client.rb
@@ -7,7 +7,7 @@ require 'socket'
 require './read_config'
 
 class Client
-  def initialize(server, id)
+  def initialize(server, id, max_routers)
     @server = server
     @id = id
 
@@ -17,10 +17,21 @@ class Client
 
     @entrada = []
     @saida = []
+    inicializa_estado(max_routers)
 
     @thr_recive.join
     @thr_send.join
     @thr_input.join
+  end
+
+  def inicializa_estado(max_routers)
+    @estado = []
+    @pack_config = ''
+    (0..max_routers - 1).each do |i|
+      @estado[i] = 0
+      @pack_config << 'L'
+    end
+    puts @pack_config
   end
 
   def new_socket
@@ -43,8 +54,10 @@ class Client
           next
         end
         socket = nil
+        @estado[@id] += 1
         socket = new_socket while socket.nil?
         socket.puts @saida.slice!(0)
+        socket.puts @estado.pack(@pack_config)
         socket.close
       end
     end
@@ -55,7 +68,11 @@ class Client
       loop do
         conn = @server.accept
         Thread.start(conn) do |c|
-          @entrada << c.gets.chomp
+          mensagem = c.gets.chomp
+          estado = c.gets.chomp
+          @entrada << { mensagem: mensagem, estado: estado.unpack(@pack_config) }
+          @estado[@id] += 1
+          atualizar_estado
         end
       end
     end
@@ -72,8 +89,11 @@ class Client
         when '1'
           puts 'Informe a mensagem:'
           @saida << $stdin.gets.chomp
+          @estado[@id] += 1
         when '2'
           show
+        when '3'
+          show_states
         else
           puts 'Informe apenas uma das opções acima!'
         end
@@ -82,24 +102,44 @@ class Client
   end
 
   def menu
+    system('clear')
     puts "Cliente [#{@id}]"
     puts '-------------- OPÇÕES --------------'
     puts '[1] Escrever mensagem'
     puts '[2] Listar mensagens'
+    puts '[3] Exibir estado'
     puts '[0] Sair'
     print '-> '
   end
 
   def show
+    system('clear')
     puts '----------------------- MENSAGENS -----------------------'
     puts @entrada
     puts '---------------------------------------------------------'
+    print 'Pressione ENTER... '
+    $stdin.gets
+  end
+
+  def show_states
+    system('clear')
+    puts '------------------------ ESTADOS ------------------------'
+    @estado.each_with_index.map { |e, i| puts "[#{i}]: #{e}" }
+    puts '---------------------------------------------------------'
+    print 'Pressione ENTER... '
+    $stdin.gets
   end
 
   def kill_threads
     Thread.kill(@thr0)
     Thread.kill(@thr1)
     Thread.kill(@thr2)
+  end
+
+  def atualizar_estado
+    temp = @entrada.last
+    temp = temp[:estado]
+    @estado = @estado.each_with_index.map { |v, i| v < temp[i] ? temp[i] : v }
   end
 end
 
@@ -119,4 +159,4 @@ config = read.get_config(id)
 server = TCPServer.open(config[0], config[1])
 
 # Instancia o cliente com seu id
-Client.new(server, id)
+Client.new(server, id, read.max_routers)
