b8e40fc55b009f47e447bc9d0b4f5aad8cc3758e
Leonardo Werlang <werlangleonardo@gmail.com>
2019-06-09 02:54:56 -0300

    :hammer: 05 | Adiciona envio de mensagens por tabela de roteamento

--- final/routing_table.rb
+++ final/routing_table.rb
@@ -3,60 +3,76 @@ require './read_config'
 module Server
   # Classe responsavel pela tabela de roteamento
   class RoutingTable
-    attr_reader :table, :config, :connections
+    attr_reader :id, :table, :config, :connections
     def initialize(id)
       @table = Hash.new { |hash, key| hash[key] = {} }
-      @config = read_config(id)
+      @id = id
+      read_config
     end
 
-    def read_config(id)
-      configure_table(id)
-      @connections = find_connections(id)
+    def read_config
+      @config = ReadConfig.new
+      configure_table
+      @connections = find_connections
     end
 
-    def print_table(id)
+    def alive(id)
+      @table[id.to_s.to_sym][:next_hop] = id
+      @table[id.to_s.to_sym][:distance] = 1
+    end
+
+    def kill(id)
+      @table[id.to_s.to_sym][:next_hop] = -1
+      @table[id.to_s.to_sym][:distance] = 4_611_686_018_427_387_902
+    end
+
+    def print_table
       puts '+-------------------------------+'
-      puts "|       Routing Table[#{id}]        |"
+      puts "|       Routing Table[#{@id}]        |"
       puts '+-------------------------------+'
       puts '|   Routers     |   Next Jump   |'
       puts '+-------------------------------+'
       @table.each do |key, router|
-        puts "|       #{key}       |        #{router[:next_jump]}      |"
+        next if key.to_s == @id.to_s || router[:next_hop] == -1
+
+        puts "|       #{key}       |        #{router[:next_hop]}      |"
       end
       puts '+-------------------------------+'
     end
 
+    def find_router(router_id)
+      router = @config.routers[router_id.to_s.to_sym]
+      [router[:ip], router[:port]]
+    end
+
+    def find_next_hop(router_id)
+      next_hop = @table[router_id.to_s.to_sym][:next_hop]
+      find_router(next_hop)
+    end
+
     private
 
-    def find_connections(id)
+    def find_connections
       connections = []
       file = File.read('connections.txt')
       con = file.split(/\n/).map { |x| x.split(':') }
       con.each do |_c0, c1|
         connections << c1.split(',')
       end
-      connections = connections[id]
+      connections = connections[@id]
       connections.map(&:to_i)
     end
 
-    def configure_table(id)
-      config = ReadConfig.new
-      initialize_table(id, config)
-    end
-
-    def initialize_table(id, config)
-      config.routers.each do |key, _router|
-        if key.to_s == id.to_s
-          @table[key.to_s.to_sym][:next_jump] = 0
+    def configure_table
+      @config.routers.each do |key, _router|
+        if key.to_s == @id.to_s
+          @table[key.to_s.to_sym][:next_hop] = 0
           next
         end
-        @table[key.to_s.to_sym][:next_jump] = -1
+        @table[key.to_s.to_sym][:next_hop] = -1
         @table[key.to_s.to_sym][:distance] = 4_611_686_018_427_387_902
         # Adicionar o tipo do roteador aqui
       end
     end
   end
 end
-
-
-Server::RoutingTable.new(0).print_table(0)
--- final/server.rb
+++ final/server.rb
@@ -6,50 +6,56 @@
 require 'socket'
 require './routing_table'
 
-class Server
-  attr_reader :id, :routing_table
-  def initialize(id)
-    @id = id
-    @routing_table = Table::RoutingTable.new(id)
+module Server
+  # Classe responsavel por enviar, reveber e reencaminhar as mensagens
+  class Router
+    attr_reader :id, :routing_table
+    def initialize(id)
+      @id = id
+      @routing_table = Server::RoutingTable.new(id)
 
-    @thr_receive = receive_msg
-    @thr_receive.join
-  end
-
-  def send_msg(send_router, controller, key, value)
-    TCPSocket.open(ip, port) do |server|
-      server.write [
-        send_router,
-        controller,
-        key.ljust(10),
-        value.ljust(30)
-      ].pack('LLA10A30')
+      @thr_receive = receive_msg
+      @thr_multicast = multicast
+      @thr_receive.join
+      @thr_multicast.join
     end
-  rescue Errno::ECONNREFUSED
-    puts 'Impossível se conectar com o servidor!'
-  end
 
-  def receive_msg
-    @thr1 = Thread.new do
-      ip = @config.routers[@id][0]
-      port = @config.routers[@id][1]
-      TCPServer.open(ip, port) do |server|
-        loop do
-          con = server.accept
-          rst = con.recv(1024).unpack('LLA10A30')
-          send_router = rst[0]
-          controller = rst[1]
-          key = rst[2]
-          value = rst[3]
-          unpack_message(send_router, controller, key, value)
-          con.close
+    def send_message(id, ip, port, hash)
+      TCPSocket.open(ip, port) do |server|
+        @routing_table.alive(id) if hash[:controller] == 1
+
+        server.write [1].pack('L')
+      end
+    rescue Errno::ECONNREFUSED
+      # puts "Impossível se conectar com o servidor! Server id: #{id} ip: #{ip} port: #{port}"
+      @routing_table.kill(id)
+      @routing_table.print_table
+    end
+
+    def receive_msg
+      Thread.new do
+        ip, port = @routing_table.find_router(@id)
+        TCPServer.open(ip, port) do |server|
+          loop do
+            con = server.accept
+            con.recv(1024).unpack('L')
+            con.close
+          end
         end
       end
     end
-  end
 
-  def kill_threads
-    Thread.kill(@thr1)
+    def multicast
+      Thread.new do
+        loop do
+          @routing_table.connections.each do |connection|
+            ip, port = @routing_table.find_router(connection)
+            send_message(connection, ip, port, controller: 1)
+          end
+          sleep(10)
+        end
+      end
+    end
   end
 end
 
@@ -60,5 +66,5 @@ end
 
 id = ARGV[0].to_i
 
-# Instancia o cliente com seu id
-Router::Server.new(id)
+# Instancia o servidor com seu id
+Server::Router.new(id)
